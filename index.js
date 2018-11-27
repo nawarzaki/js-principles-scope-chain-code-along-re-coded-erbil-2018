8c1b75a0a038cbfae322d74fc2abc263940be927
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-27 08:39:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,8 @@ function changeLeastFavoriteCustomer(){
   leastFavoriteCustomer = 'sour fred'
 }
 
-function attemptTwoFavoriteCustomers() {
-  let favoriteCustomer = 'bar'
-   let favoriteCustomer = 'not bar'
+function congratulateCustomer(){
+  return `congrats ${favoriteCustomer}`
 }
-return favoriteCustomer
-}
+
+
